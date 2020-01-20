2644db02d7f16414f3426009f9c3df527f65138c
Daniel Gomez-Sierra <danielgomezsierra@gmail.com>
2020-01-20 12:15:26 -0500

    Add grouping queries for articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,4 +7,16 @@ class Article < ApplicationRecord
   scope :by_name_or_text, ->(keyword) do
     where("name ILIKE :keyword OR content ILIKE :keyword", keyword: "%#{keyword}%")
   end
+
+  scope :group_by_story, -> { includes(:story).group_by(&:story) }
+  scope :group_by_story_count, -> { includes(:story).group(:story).count }
+  scope :group_by_category_count, -> {
+    joins(:story).group('stories.id', 'stories.name', :category).count.sort
+  }
+  scope :group_by_story_last_created, -> {
+    story_articles_hash = order(created_at: :desc).group_by(&:story)
+    story_articles_hash.each do |story, articles|
+      story_articles_hash[story] = articles.first
+    end
+  }
 end
--- app/services/article_management_service.rb
+++ app/services/article_management_service.rb
@@ -6,7 +6,9 @@ class ArticleManagementService
 
   def call
     @articles = search_by_name_or_text
-    @articles = sort_by
+    @articles = sort_by_field
+    @articles = group_by_field
+    @articles = group_by_story_with_totals
     @articles
   end
 
@@ -19,18 +21,34 @@ class ArticleManagementService
     @articles.by_name_or_text(keyword)
   end
 
-  def sort_by
+  def sort_by_field
     sort_field = @search_params[:sort]
     return @articles unless sort_field
 
     @articles.order(sort_field)
   end
 
-  def group_by(field)
+  def group_by_field
+    group_field = @search_params[:group]
+    return @articles unless group_field
 
+    if group_field.to_sym == :story
+      @articles.group_by_story
+    else
+      @articles.group_by(&:"#{group_field}")
+    end
   end
 
-  def group_by_with_totals(field)
+  def group_by_story_with_totals
+    group_totals = @search_params[:group_totals]&.to_sym
+    return @articles unless group_totals
 
+    if group_totals == :article
+      @articles.group_by_story_count
+    elsif group_totals == :article_type
+      @articles.group_by_category_count
+    elsif group_totals == :last_created
+      @articles.group_by_story_last_created
+    end
   end
 end
